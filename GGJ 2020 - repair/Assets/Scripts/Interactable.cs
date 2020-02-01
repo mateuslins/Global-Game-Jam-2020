3efee2975c69cb76a9baef532b9f8bdd1398ae0e
Mateus Lins <41246021+mateuslins@users.noreply.github.com>
2020-02-01 15:44:33 -0300

    movimentação de personagem e coleta e spawn de items

---
+++ GGJ 2020 - repair/Assets/Scripts/Interactable.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+//[RequireComponent(typeof(ColorOnHover))]
+public class Interactable : MonoBehaviour
+{
+    public PlayerController player;
+    public float radius = 3f;
+
+    private Vector2 screenBounds;
+
+    void Awake()
+    {
+        player = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            player.CollectPiece();
+            Destroy(gameObject);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
---
+++ GGJ 2020 - repair/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D body;
+
+    //inventory
+    public static int pieces = 0;
+
+    //movement
+    float horizontal;
+    float vertical;
+    float moveLimiter = 0.7f;
+
+    public float runSpeed = 20.0f;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        if (horizontal != 0 && vertical != 0)
+        {
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
+
+        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+    
+    public void CollectPiece()
+    {
+        pieces += 1;
+        Debug.Log("O numero de peças é " + pieces);
+    }
+
+    public void DropPieces()
+    {
+        pieces = 0;
+        Debug.Log("Depositou todas as peças na torre");
+    }
+}
